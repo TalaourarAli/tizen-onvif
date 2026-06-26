9d09b6f18e63cb5fa0b283fb558596a43da3dbe4
Ali TALAOURAR <talaourarfr@gmail.com>
2026-06-26 21:39:59 +0200

    new url

--- service.js
+++ service.js
@@ -4,12 +4,12 @@ const url = require('url');
 // Crée un mini serveur local pour communiquer avec ton interface HTML
 const server = http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    
+
     const parsedUrl = url.parse(req.url, true);
-    
+
     if (parsedUrl.pathname === '/get-stream') {
         const query = parsedUrl.query;
-        const ip = query.ip || '192.168.1.50';
+        const ip = query.ip || '192.168.10.103';
         const port = query.port || '554';
         const user = query.user || 'admin';
         const pass = query.pass || 'password';
@@ -25,8 +25,9 @@ const server = http.createServer((req, res) => {
         const portSuffix = port ? `:${port}` : '';
 
         // Logique ONVIF dynamique ou construction directe du flux
-        const streamUrl = `rtsp://${user}:${pass}@${ip}${portSuffix}/${normalizedPath}`;
-        
+        // const streamUrl = `rtsp://${user}:${pass}@${ip}${portSuffix}/${normalizedPath}`;
+        const streamUrl = `rtsp://${user}:${pass}@${ip}${portSuffix}/cam/realmonitor?channel=1&subtype=1`
+
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({ streamUrl }));
     } else {
